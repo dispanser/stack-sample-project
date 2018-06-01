4e04b7b449efe4c81c77e0ae889e5eaa2e131ce5
Thomas Peiselt <thomas.peiselt@tomtom.com>
2018-06-01 16:12:23 +0200

    Added example for hspec test.

---
+++ spec/Spec.hs
@@ -0,0 +1,14 @@
+import Lib (function1)
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+  spec
+
+spec :: Spec
+spec = do
+  describe "basic spec description" $ do
+    context "some more detailed context" $ do
+      it "concrete test description" $ function1 [1, 2, 3] `shouldBe` [3, 2, 1]
+
+
