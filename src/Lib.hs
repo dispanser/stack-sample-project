de80cee86b25aa9d023f8e94a31f3ec756a77a4f
Thomas Peiselt <thomas.peiselt@tomtom.com>
2018-06-05 14:37:18 +0200

    Adding info on how to use the debug package.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,19 @@
-module Lib where
+{-# LANGUAGE TemplateHaskell, ViewPatterns, PartialTypeSignatures #-}
+module Lib
+  ( function1
+  , fun1debug
+  )
+where
+
+import Debug
 
 -- broken reverse function that doesn't work on singleton lists
 function1 :: [a] -> [a]
 function1 [_] = []
-function1 xs = reverse xs
+function1 xs  = reverse xs
+
+debug [d|
+  fun1debug :: [a] -> [a]
+  fun1debug []     = []
+  fun1debug (x:xs) = fun1debug xs ++ [x]
+  |]
